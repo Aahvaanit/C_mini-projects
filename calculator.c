08e9b732d3f6428fe0e048095c85658ba2f36d33
Aahvaanit <tripathi.aahvaanit@gmail.com>
2023-05-06 19:56:18 +0530

    complete

--- calculator.c
+++ calculator.c
@@ -2,23 +2,50 @@
 #include <stdlib.h>
 
 int main(void) {
-    printf(">>>>THE CALCULATOR<<<<\n");
-    printf(">>Please enter your first name: ")
+    printf(">>>>  THE CALCULATOR  <<<<\n");
+    printf(">  Please enter your first name: ");
 
     char name[20];
-    scanf("%s\n", name);
+    scanf("%[^\n]%*c", name);
 
-    printf(">>Welcome %s!\n", name);
+    printf(">  Welcome %s!\n\n", name);
 
-    printf("Press: [1] for addition\n");
-    printf("       [2] for subtraction\n");
-    printf("       [3] for multiplication\n");
-    printf("       [4] for division\n");
-    printf("       [5] for exponentiation\n");
-    printf("       [6] to stop expression\n\n");
+    
 
-    int operator, input;
+
+    printf(">  Press: [1] for addition\n");
+    printf("          [2] for subtraction\n");
+    printf("          [3] for multiplication\n");
+    printf("          [4] for division\n");
+    printf("          [5] for exponentiation\n");
+    printf("          [6] to stop expression\n\n");
+
+
+    int num1, num2, operator;
     do {
-        scanf("%i ", &n);
-    } while (n != 6);
+        int calc, holder;
+        printf(">  Enter the numbers: ");
+        scanf("%i  %i\n", &num1, &num2);
+
+        printf(">  Enter your desired operation: ");
+        scanf("%i ", &operator);
+
+        switch (operator) {
+            case 1:
+                calc = num1 + num2;
+            case 2:
+                calc = num1 - num2;
+            case 3:
+                calc = num1 * num2;
+            case 4:
+                calc = num1 / num2;
+            case 5:
+                for (int i = 0; i < num2; i++) {
+                    holder *= num1;
+                }
+                calc = holder;
+        }
+
+        printf("%i\n", calc);
+    } while (operator != 6);
 }
